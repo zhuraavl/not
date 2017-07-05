b6b9a683b600999379d8a74435441b743313c8db
zhuraavl <zhuraavl@gmail.com>
2017-07-05 13:36:35 +0300

    update header

--- js/lib/scripts.js
+++ js/lib/scripts.js
@@ -37,27 +37,6 @@ $(document).ready(function () {
 
 
 
-  $(document).on('click', '.brand-links a[href^="#"]', function (e) {
-    // target element id
-    var id = $(this).attr('href');
-
-    // target element
-    var $id = $(id);
-    if ($id.length === 0) {
-      return;
-    }
-
-    // prevent standard hash navigation (avoid blinking in IE)
-    e.preventDefault();
-
-    // top position relative to the document
-    var pos = $id.offset().top - 0;
-
-    // animated top scrolling
-    $('body, html').animate({
-      scrollTop: pos
-    });
-  });
 
 
   $(".open-search").click(function () {
